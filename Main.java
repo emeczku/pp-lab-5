2e98d4c9b3277f00bccadc83df4697afee15e781
Przemek Majewski <dzius89@gmail.com>
2024-05-05 14:34:55 +0200

    feat: create email messenger

---
+++ Main.java
@@ -0,0 +1,18 @@
+import company.beans.Person;
+import company.exceptions.InvalidAgeException;
+import company.implementations.EmailMessenger;
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+            Person person = new Person("John", 30);
+            System.out.println("Name: " + person.getName());
+            System.out.println("Age: " + person.getAge());
+        } catch (InvalidAgeException e) {
+            System.out.println("Invalid age: " + e.getMessage());
+        }
+
+        EmailMessenger emailMessenger = new EmailMessenger();
+        emailMessenger.sendMessage("Person data has been displayed successfully.");
+    }
+}
