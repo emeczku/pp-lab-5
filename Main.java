4f35c19ebe3fb2a8eec01cfdcd96042fefe2f156
Przemek Majewski <dzius89@gmail.com>
2024-05-05 14:45:08 +0200

    feat: refactor Main.java to handle multiple persons && calculate addition results

--- Main.java
+++ Main.java
@@ -5,19 +5,25 @@ import company.utils.MathUtils;
 
 public class Main {
     public static void main(String[] args) {
+        final int b = 10;
+
+        Person[] people = new Person[5];
+
         try {
-            Person person = new Person("John", 30);
-            System.out.println("Name: " + person.getName());
-            System.out.println("Age: " + person.getAge());
+            people[0] = new Person("John Doe", 30);
+            people[1] = new Person("Tom Ellis", 27);
+            people[2] = new Person("Bob Marlin", 41);
+            people[3] = new Person("Emily Clark", 33);
+            people[4] = new Person("Jack Erickson", 22);
+
+            for (Person person : people) {
+                int result = MathUtils.add(person.getAge(), b);
+
+                EmailMessenger emailMessenger = new EmailMessenger();
+                emailMessenger.sendMessage(person.getName() + " - Result of addition is: " + result);
+            }
         } catch (InvalidAgeException e) {
             System.out.println("Invalid age: " + e.getMessage());
         }
-
-        int result = MathUtils.add(5, 7);
-        System.out.println("Result of addition: " + result);
-
-        EmailMessenger emailMessenger = new EmailMessenger();
-        emailMessenger.sendMessage("Person data has been displayed successfully.");
-        emailMessenger.sendMessage("Result of addition: " + result);
     }
 }
